825a1fa836b5690da384a3d942c08ea89ebbf84b
Šárka Manďáková <sarka.mandakova@gmail.com>
2024-06-08 09:20:36 +0200

    Druhý commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,45 @@
 import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage';
 import './global.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(
-  document.querySelector('#app'),
-).render(<HomePage />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: 'anglictina',
+        element: <Anglictina />,
+      },
+      {
+        path: 'spanelstina',
+        element: <Spanelstina />,
+      },
+      {
+        path: 'portugalstina',
+        element: <Portugalstina />,
+      },
+      {
+        path: 'anglictina/cviceni',
+        element: <AnglictinaCviceni />,
+      },
+      {
+        path: 'spanelstina/cviceni',
+        element: <SpanelstinaCviceni />,
+      },
+      {
+        path: 'portugalstina/cviceni',
+        element: <PortugalstinaCviceni />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.querySelector('#app')).render(
+  <RouterProvider router={router} />,
+);
