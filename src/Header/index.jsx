d9e5338e3fa1387aee36362cfbc918720241fce7
Vendula Hůrková <hurkova.vendula@gmail.com>
2024-06-12 18:32:19 +0200

    responzivita (npm install react-icons)

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-const MenuItem = ({to, itemName}) => {
-  return(
-      <li> 
-        <Link to={to} className="font-medium text-lg text-black hover:underline"> {itemName} </Link>
-       </li>
-  )
-}
+const MenuItem = ({ to, itemName, onClick }) => {
+  return (
+    <li>
+      <Link to={to} className="font-medium text-lg text-black hover:underline" onClick={onClick}>
+        {itemName}
+      </Link>
+    </li>
+  );
+};
 
+export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export const Header = () => (
-  <header className='flex items-center justify-evenly bg-yellow-400 py-8 '>
-  <p className='text-5xl font-medium mr-80 '> POJĎ se UČIT </p>
-  <nav>
-    <ul className="flex space-x-4 ">
-      <MenuItem itemName={"Úvod"} to={"/"}/>
-      <MenuItem itemName={"Angličtina"} to={"/anglictina"}/>
-      <MenuItem itemName={"Španělština"} to={"/spanelstina"}/>
-      <MenuItem itemName={"Portugalština"} to={"/portugalstina"}/>
-      <MenuItem itemName={"AJ-cvičení"} to={"/anglictina/cviceni"}/>
-      <MenuItem itemName={"ŠJ-cvičení"} to={"/spanelstina/cviceni"}/>
-      <MenuItem itemName={"PTG-cvičení"} to={"/portugalstina/cviceni"}/>
-    </ul>
-  </nav>
-  </header>
-);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
+  const handleCloseMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <header className="flex items-center justify-between bg-yellow-400 py-4 px-6">
+      <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium">POJĎ se UČIT</p>
+      <div className="block lg:hidden">
+        <button onClick={handleToggle} className="text-black focus:outline-none">
+          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+      </div>
+      <nav className={`lg:flex items-center ${isOpen ? 'block' : 'hidden'}`}>
+        <ul className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-4">
+          <MenuItem itemName={"Úvod"} to={"/"} onClick={handleCloseMenu} />
+          <MenuItem itemName={"Angličtina"} to={"/anglictina"} onClick={handleCloseMenu} />
+          <MenuItem itemName={"Španělština"} to={"/spanelstina"} onClick={handleCloseMenu} />
+          <MenuItem itemName={"Portugalština"} to={"/portugalstina"} onClick={handleCloseMenu} />
+          <MenuItem itemName={"AJ-cvičení"} to={"/anglictina/cviceni"} onClick={handleCloseMenu} />
+          <MenuItem itemName={"ŠJ-cvičení"} to={"/spanelstina/cviceni"} onClick={handleCloseMenu} />
+          <MenuItem itemName={"PTG-cvičení"} to={"/portugalstina/cviceni"} onClick={handleCloseMenu} />
+        </ul>
+      </nav>
+    </header>
+  );
+};
