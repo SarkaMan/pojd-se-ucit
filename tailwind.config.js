0b2b534c8b5d2a17bf32c593a844fd4cc608e8de
Vendula Hůrková <hurkova.vendula@gmail.com>
2024-06-14 15:12:39 +0200

    změna fontu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,md,mdx}'],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         heading: 'Nunito Sans, sans-serif',
+//         base: 'Nunito Sans, sans-serif',
+//       },
+//     },
+//   },
+//   plugins: [require('@tailwindcss/forms')],
+// };
+
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,md,mdx}'],
   theme: {
     extend: {
       fontFamily: {
-        heading: 'Nunito Sans, sans-serif',
-        base: 'Nunito Sans, sans-serif',
+        heading: 'Embed code, sans-serif',
+        base: 'Embed code, sans-serif',
       },
     },
   },
